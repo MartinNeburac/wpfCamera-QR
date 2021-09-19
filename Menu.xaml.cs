a9cc05e438473b6025bae1568ab4aa1cd7095cac
martinneburac <58069937+MartinNeburac@users.noreply.github.com>
2021-09-19 22:32:38 +0200

    Add files via upload

---
+++ Menu.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+
+namespace WpfImageProcessing
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+
+    
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //StartCamera();
+            MainWindow mW = new MainWindow(this);
+            mW.Show();
+            this.Hide();
+            
+            
+        }
+        
+        
+    }
+}
+
